864108d3e65850f6d0ecbc84fca39129f9b1ac8a
Shulepov Michael <nemihail02@gmail.com>
2025-11-09 16:18:02 +0700

    working code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 
+import tkinter.messagebox
+from time import sleep
 import pygetwindow
-import pyautogui
-import time
+import datetime
 
-# ДОПИШИ СЕГОДНЯ ВЕЧЕРОМ
-# сори что накричал я забы выключить капс
+# made by ne dynamics, tg: @shulepok
+
+while True:
+    try:
+        hours = datetime.datetime.now().hour
+        need_to_close = pygetwindow.getWindowsWithTitle('FireFox')
+        if hours >= 23:
+            if need_to_close:
+                need_to_close = need_to_close[0]
+                need_to_close.close()
+                tkinter.messagebox.showwarning('sleepwell', 'youre gonna sleep, not youtube!')
+        sleep(1)
+    except KeyboardInterrupt:
+        pass
